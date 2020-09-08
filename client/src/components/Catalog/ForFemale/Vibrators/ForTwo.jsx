d116b021d664b18fe99829785095c2bc9944bedc
Alex-La <55190911+Alex-La@users.noreply.github.com>
2020-09-09 01:38:15 +0300

    Vibrators category

---
+++ client/src/components/Catalog/ForFemale/Vibrators/ForTwo.jsx
@@ -0,0 +1,158 @@
+import React, { Fragment, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { useQuery } from "@apollo/react-hooks";
+import PRODUCTS_QUERY from "../../../../graphql/queries/products";
+
+import Filter from "../../../assets/Filter/Filter";
+import ProductCont from "../../../assets/Product/ProductCont";
+import LoadMoreButton from "../../../assets/Filter/LoadMoreButton";
+import FixedToCartButton from "../../../assets/FloatActionBtn/FixedToCartButton";
+
+const catalogData = [
+  {
+    path: "/catalog/for-female/vibrators/hi-tech",
+    name: "Hi-tech",
+  },
+  {
+    path: "/catalog/for-female/vibrators/anal-vaginal-vibrators",
+    name: "Анально-вагинальные",
+  },
+  {
+    path: "/catalog/for-female/vibrators/big-vibrators",
+    name: "Большие вибраторы",
+  },
+  {
+    path: "/catalog/for-female/vibrators/vibration-kits",
+    name: "Вибронаборы",
+  },
+  {
+    path:
+      "/catalog/for-female/vibrators/vibration-stimulators-and-vibration-bullets",
+    name: "Вибростимуляторы, вибропульки",
+  },
+  {
+    path: "/catalog/for-female/vibrators/waterproof",
+    name: "Водонепроницаемые",
+  },
+  {
+    path: "/catalog/for-female/vibrators/rotating",
+    name: "Вращающиеся",
+  },
+  {
+    path: "/catalog/for-female/vibrators/bilateral-vibrators",
+    name: "Двусторонние",
+  },
+  {
+    path: "/catalog/for-female/vibrators/for-two",
+    name: "Для двоих",
+  },
+  {
+    path: "/catalog/for-female/vibrators/classic",
+    name: "Классические",
+  },
+  {
+    path: "/catalog/for-female/vibrators/computer-type",
+    name: "Компьтерного типа",
+  },
+  {
+    path: "/catalog/for-female/vibrators/realistic-vibrators",
+    name: "Реалистики",
+  },
+  {
+    path: "/catalog/for-female/vibrators/with-radio-control",
+    name: "С радиоуправлением",
+  },
+  {
+    path: "/catalog/for-female/vibrators/with-clitoris-stimulator",
+    name: "Со стимулятором клитора",
+  },
+  {
+    path: "/catalog/for-female/vibrators/g-spot-stimulants-vibrators",
+    name: "Стимуляторы G-точки",
+  },
+];
+
+const ForTwo = () => {
+  const [loadingOnButton, setLoadingOnButton] = useState(false);
+  const [priceRange, setPriceRange] = useState([]);
+  const [radio, setRadio] = useState("block");
+  const [orderBy, setOrderBy] = useState("DEFAULT");
+
+  const { data, loading, fetchMore, refetch } = useQuery(PRODUCTS_QUERY, {
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      subsection: "Для двоих ",
+      orderBy,
+      priceRange: priceRange.length !== 0 ? priceRange : [],
+    },
+  });
+
+  return (
+    <Fragment>
+      <div className="section">
+        <div className="row">
+          <div className="col s12">
+            <ul className="breadcrumb">
+              <li>
+                <Link to="/" className="orange-text">
+                  Главная страница
+                </Link>
+              </li>
+              <li>
+                <Link to="/catalog" className="orange-text">
+                  Каталог
+                </Link>
+              </li>
+              <li>
+                <Link to="/catalog/for-female" className="orange-text">
+                  Женщинам
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/catalog/for-female/vibrators"
+                  className="orange-text"
+                >
+                  {window.innerWidth <= 425 && <br />}
+                  Вибраторы
+                </Link>
+              </li>
+              <li className="black-text">Для двоих</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <Filter
+        setLoadingOnButton={setLoadingOnButton}
+        catalogData={catalogData}
+        setPriceRange={setPriceRange}
+        radio={radio}
+        setRadio={setRadio}
+        setOrderBy={setOrderBy}
+        price_range={data && data.products && data.products.price_range}
+      />
+
+      <ProductCont
+        data={data && data.products && data.products.products}
+        refetch={data && data.products.refetch_require && refetch}
+        loading={loading}
+        loadingOnButton={loadingOnButton}
+        radio={radio}
+      />
+
+      <LoadMoreButton
+        cursor={data && data.products && data.products.cursor}
+        hasMore={data && data.products && data.products.hasMore}
+        loading={loading}
+        setLoadingOnButton={setLoadingOnButton}
+        fetchMore={fetchMore}
+      />
+
+      <FixedToCartButton />
+    </Fragment>
+  );
+};
+
+export default ForTwo;
