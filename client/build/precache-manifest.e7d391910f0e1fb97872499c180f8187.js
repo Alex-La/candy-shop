eb9c43117439ed69198af0fabfce00c03fe6ac5b
Alex-La <lars6162@gmail.com>
2020-11-30 23:44:46 +0300

    Build

---
+++ client/build/precache-manifest.e7d391910f0e1fb97872499c180f8187.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "559c22e688a49b860939a0a29c9edf6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d47348e93a8ee409a5",
+    "url": "/static/css/2.5e993506.chunk.css"
+  },
+  {
+    "revision": "50e0e933ea7ee81a1350",
+    "url": "/static/css/main.abce7164.chunk.css"
+  },
+  {
+    "revision": "30d47348e93a8ee409a5",
+    "url": "/static/js/2.d2bff184.chunk.js"
+  },
+  {
+    "revision": "35d97e17a1e81e10b7c3ce6a2cbc5c9b",
+    "url": "/static/js/2.d2bff184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e0e933ea7ee81a1350",
+    "url": "/static/js/main.60e77dcd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "f2f94f92bb510c54d29e8b38301b3a26",
+    "url": "/static/media/adult.f2f94f92.svg"
+  },
+  {
+    "revision": "021ee9a3c700f0a7a4b5e53007426a1e",
+    "url": "/static/media/logo.021ee9a3.svg"
+  }
+]);
