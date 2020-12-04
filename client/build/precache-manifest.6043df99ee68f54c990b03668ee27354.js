6e3d0d760f0967d90ef35b4c46229625f5d85362
Alex-La <55190911+Alex-La@users.noreply.github.com>
2020-12-04 19:20:18 +0300

    build

---
+++ client/build/precache-manifest.6043df99ee68f54c990b03668ee27354.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "702d96375be88cd05c6ee6fae06f117e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3d075a790572c2fd11e",
+    "url": "/static/css/2.5e993506.chunk.css"
+  },
+  {
+    "revision": "03d6b38e5935e047c758",
+    "url": "/static/css/main.42e39a26.chunk.css"
+  },
+  {
+    "revision": "c3d075a790572c2fd11e",
+    "url": "/static/js/2.32d75832.chunk.js"
+  },
+  {
+    "revision": "35d97e17a1e81e10b7c3ce6a2cbc5c9b",
+    "url": "/static/js/2.32d75832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03d6b38e5935e047c758",
+    "url": "/static/js/main.c8828471.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "f2f94f92bb510c54d29e8b38301b3a26",
+    "url": "/static/media/adult.f2f94f92.svg"
+  },
+  {
+    "revision": "021ee9a3c700f0a7a4b5e53007426a1e",
+    "url": "/static/media/logo.021ee9a3.svg"
+  }
+]);
